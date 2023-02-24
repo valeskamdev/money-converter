ed7f4e06d5e1cabe8783d24e4a4632a3bdb34833
valeskamdev <valeskkitty@gmail.com>
2023-02-24 14:29:08 -0300

    promises and async awaint

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
 const button = document.getElementById("convert-button");
 const select = document.getElementById("currency-select");
 
-const dollar = 5.22;
-const euro = 5.58;
-const india = 0.063;
-const korea = 0.0041;
-const china = 0.77;
-
-const convertValues = () => {
+const convertValues = async () => {
   const realValue = document.getElementById("real-value");
   const currencyValue = document.getElementById("currency-value");
   const inputReal = document.getElementById("input-real").value;
-  
-  const realToDollar = inputReal / dollar;
-  const realToEuro = inputReal / euro;
-  const realToIndia = inputReal / india;
-  const realToKorea = inputReal / korea;
-  const realToChina = inputReal / china;
 
-  console.log(realToChina);
+  const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
+
+  const dolar = data.USDBRL.high
+  const euro = data.EURBRL.high
+  const bitcoin = data.BTCBRL.high
 
   realValue.innerHTML = new Intl.NumberFormat("pt-BR", {
     style: "currency",
@@ -29,35 +21,22 @@ const convertValues = () => {
     currencyValue.innerHTML = new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD",
-    }).format(realToDollar);
+    }).format(inputReal / dolar);
   }
 
   if (select.value === "€ Euro") {
     currencyValue.innerHTML = new Intl.NumberFormat("de-DE", {
       style: "currency",
       currency: "EUR",
-    }).format(realToEuro);
+    }).format(inputReal / euro);
   }
 
-  if (select.value === "₹ India") {
-    currencyValue.innerHTML = new Intl.NumberFormat("en-IN", {
+  if (select.value === "Ƀ Bitcoin") {
+    currencyValue.innerHTML = new Intl.NumberFormat("en-US", {
       style: "currency",
-      currency: "INR",
-    }).format(realToIndia);
-  }
-
-  if (select.value === "₩ Korea") {
-    currencyValue.innerHTML = new Intl.NumberFormat("ko-KR", {
-      style: "currency",
-      currency: "KRW",
-    }).format(realToKorea);
-  }
-
-  if (select.value === "¥ China") {
-    currencyValue.innerHTML = new Intl.NumberFormat("zh-CN", {
-      style: "currency",
-      currency: "CNY",
-    }).format(realToChina);
+      currency: "BTC",
+      minimumFractionDigits: 8
+    }).format(inputReal / bitcoin).replace(/BTC/,'Ƀ');
   }
 };
 
@@ -75,43 +54,14 @@ const changeCurrency = () => {
     currencyImg.src = "assets/euro.svg";
   }
 
-  if (select.value === "₹ India") {
-    currencyName.innerHTML = "India";
-    currencyImg.src = "assets/india.svg";
-  }
-
-  if (select.value === "₩ Korea") {
-    currencyName.innerHTML = "Korea";
-    currencyImg.src = "assets/korea.svg";
-  }
-
-  if (select.value === "¥ China") {
-    currencyName.innerHTML = "China";
-    currencyImg.src = "assets/china.svg";
+  if (select.value === "Ƀ Bitcoin") {
+    currencyName.innerHTML = "Bitcoin";
+    currencyImg.src = "assets/bitcoin.svg";
   }
   convertValues();
 };
 
-function validateMonetaryValue(event) {
-  const key = event.key;
-  const value = event.target.value;
-
-  const numericValue = parseFloat(value.replace(/[,.]/g, ''));
-
-  if (key === 'Backspace' || key === 'Delete') {
-    return true;
-  }
-
-  if ((key >= '0' && key <= '9')) {
-    return true;
-  }
-
-  event.preventDefault();
-  return false;
-}
-
 const inputReal = document.getElementById("input-real");
 
-inputReal.addEventListener('keydown', validateMonetaryValue);
 button.addEventListener("click", convertValues);
 select.addEventListener("change", changeCurrency);
